97ba96d42e42a44e7e9b4bb149593ef72ea35f77
Vini7720 <143711046+Vini7720@users.noreply.github.com>
2023-11-06 23:38:30 -0300

    Update Questao 7.c

--- Questao 7.c
+++ Questao 7.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -14,6 +6,7 @@ Welcome to GDB Online.
 int escolha;
 int main()
 {
+
     printf("Escolha uma opcao\n");
     printf("1 - Novo jogo \n");
     printf("2 - Carregar jogo \n");
@@ -23,11 +16,11 @@ int main()
     switch (escolha)
 {
 case 1:
-   printf ("Iniciando um novo jogo:\n");
+   printf ("Comecando um jogo novo:\n");
     break;
     
     case 2:
-   printf ("Carregando um jogo em andamento:\n");
+   printf ("Levando um jogo em andamento:\n");
    
     break;
     
@@ -36,7 +29,7 @@ case 1:
     break;
 
 default:
-printf ("Opcao invalida!");
+printf ("A opcao sera invalida!");
     break;
 }
 
