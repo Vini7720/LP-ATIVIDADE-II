4983c5ad1a34bf960b1f89e67db25c84598074c6
Vini7720 <143711046+Vini7720@users.noreply.github.com>
2023-11-06 23:35:23 -0300

    Update Questao 4.c

--- Questao 4.c
+++ Questao 4.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -19,7 +11,7 @@ int main()
   setlocale (LC_ALL, "Portuguese");
 
   
-  printf ("Qual a sua idade\n");
+  printf ("Qual idade voce tem\n");
   scanf ("%i",&idade);
   
   if (idade >=18)
