967e6163a85ebce96262759943fa89f2da1e22ca
Vini7720 <143711046+Vini7720@users.noreply.github.com>
2023-11-06 23:28:07 -0300

    Update Questao 10.c

--- Questao 10.c
+++ Questao 10.c
@@ -1,12 +1,4 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <stdio.h>
+/#include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
@@ -19,17 +11,18 @@ int soma;
 
 int main ()
 {
-  setlocale (LC_ALL, "");
 
-  printf ("Digite o primeiro numero:\n");
+  setlocale (LC_ALL, portuguese"");
+
+  printf ("Escreva o primeiro numero:\n");
   scanf ("%i", &n1);
 
-  printf ("Digite o segundo numero:\n");
+  printf ("Escreva o segundo numero:\n");
   scanf ("%i", &n2);
 
-  printf ("Digite a operacao:\n");
-  printf ("Digite 1 para operacao: ( - )\n");
-  printf ("Digite 2 para operacao: ( + )\n");
+  printf ("Coloque a atividade:\n");
+  printf ("Coloque o numero 1 para atividade: ( - )\n");
+  printf ("Coloque o numero 2 para atividade: ( + )\n");
   scanf ("%i", &escolha);
 
   sub1 = n1-n2;
@@ -37,16 +30,16 @@ int main ()
 switch (escolha)
 {
 case 1:
-  printf("A subtracao do numero %i e %i e:%i\n", n1, n2, sub1);
+  printf("A subtracao dos numeros %i e %i e:%i\n", n1, n2, sub1);
   
   break;
 
   case 2:
-  printf("A soma dos numeros %i e %i e:%i\n", n1, n2, soma );
+  printf("A adicao dos numeros %i e %i e:%i\n", n1, n2, soma );
   break;
 
 default:
-printf("A opcao e invalida!\n");
+printf("Essa opcao ficara errada !\n");
   break;
 }
   return 0;
