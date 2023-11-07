ae1f54e758cad2a0ba2276afcb139f4839cf48ae
Vini7720 <143711046+Vini7720@users.noreply.github.com>
2023-11-06 23:37:47 -0300

    Update Questao 6.c

--- Questao 6.c
+++ Questao 6.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -14,31 +6,32 @@ Welcome to GDB Online.
 int escolha;
 int main()
 {
+
     printf("Escolha um produto\n");
-    printf("1 - Camisa R$20,00\n");
-    printf("2 - Calca R$40,00\n");
-    printf("3 - Sapato R$70,00\n");
+    printf("1 - Camisa R$30,00\n");
+    printf("2 - Calca R$50,00\n");
+    printf("3 - Sapato R$80,00\n");
     scanf ("%i",&escolha);
     
     switch (escolha)
 {
 case 1:
    printf ("Produto Camisa:\n");
-    printf ("Custa R$ 20,00.\n");
+    printf ("Valor R$ 30,00.\n");
     break;
     
     case 2:
    printf ("Produto Calca:\n");
-    printf ("Custa R$ 40,00.\n");
+    printf ("Valor R$ 50,00.\n");
     break;
     
     case 3:
    printf ("Produto Sapato:\n");
-    printf ("Custa R$ 70,00.\n");
+    printf ("Valor R$ 80,00.\n");
     break;
 
 default:
-printf ("Opcao invalida!");
+printf ("A opcao sera invalida!");
     break;
 }
 
