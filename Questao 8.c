7d086eb43f8901b75dbf51e582a32e432cd49c12
Vini7720 <143711046+Vini7720@users.noreply.github.com>
2023-11-06 23:41:16 -0300

    Update Questao 8.c

--- Questao 8.c
+++ Questao 8.c
@@ -1,34 +1,32 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 
 int
 main ()
+
 {
   int numero;
 
 
-  printf ("Digite um numero: ");
+  printf ("Escreva 1 numero ");
   scanf ("%d", &numero);
 
 if (numero < 0)
     {
-      printf ("Numero invalido.\n");
+
+      printf ("Numero errado.\n");
       return 0;
+
     }
 
   while (numero >= 0)
     {
+
+
       printf ("%d\n", numero);
       numero--;
+
     }
 
 
